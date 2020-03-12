b53ac80ac1e3e35362bcf95762b509403c1af8fa
xiaojin <33309882+xiaojin525@users.noreply.github.com>
2020-03-12 17:40:25 +0800

    Update recreate.sh

--- gitlab-ci/recreate.sh
+++ gitlab-ci/recreate.sh
@@ -1,6 +1,6 @@
 yamls=`ls|grep yaml |xargs`
 for y in $yamls;
 do
-  kubectl delete -f $y
-  kubectl create -f $y
+  kubectl delete -f $y -n tekton-pipelines
+  kubectl create -f $y -n tekton-pipelines
 done
