d039f62e72f3c533500c1c22ef2b53b8e15faead
lu.jin(卢进) <1395856829@qq.com>
2020-03-26 18:09:02 +0800

    add delete pipelinerun cronjob

--- cronjob/dockerfile/delete-pipelinerun.sh
+++ cronjob/dockerfile/delete-pipelinerun.sh
@@ -1,5 +1,12 @@
-#! /bin/sh
+#!/bin/sh
 
 namespace=$1
 COMPLETIONTIME=$2
-kubectl get pipelineruns -n $namespace |grep -v NAME|awk '{if ($5 == '$COMPLETIONTIME') print $1}'|xargs kubectl delete pipelineruns -n $namespace
+echo "RUN: delete pipelineruns starting"
+kubectl get pipelineruns -n $namespace |grep -v NAME|awk '{if ($5 ~ /^'$COMPLETIONTIME'/) print $1}'|xargs kubectl delete pipelineruns -n $namespace 2> /dev/null
+if [ $? -eq 0 ]; then
+    echo "RUN: delete completed"
+else
+
+    echo "RUN: delete completed: no pipelineruns COMPLETIONTIME ~" $COMPLETIONTIME
+fi
